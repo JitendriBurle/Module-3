c2d78d5fd1d756d85d7d4830851b686beb04fa01
JitendriBurle <ji28tu@gmail.com>
2025-12-19 19:54:43 +0530

    Add files via upload

---
+++ useEffect/counter-app/src/Counter.js
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from "react";
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    if (count !== 0 && count % 3 === 0) {
+      alert(`The current number ${count} is divisible by 3`);
+    }
+  }, [count]); // runs only when count changes
+
+  return (
+    <div>
+      <h2>Counter: {count}</h2>
+      <button onClick={() => setCount(count + 1)}>
+        Increase
+      </button>
+    </div>
+  );
+}
+
+export default Counter;
