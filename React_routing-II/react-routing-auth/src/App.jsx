621550fef7d8d9c0dbaea19e105b1a666224034b
JitendriBurle <ji28tu@gmail.com>
2025-12-25 18:40:02 +0530

    Add files via upload

---
+++ React_routing-II/react-routing-auth/src/App.jsx
@@ -0,0 +1,33 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Todos from "./pages/Todos";
+import TodoDetails from "./pages/TodoDetails";
+import ProtectedRoute from "./ProtectedRoute";
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+
+      <Route
+        path="/todos"
+        element={
+          <ProtectedRoute>
+            <Todos />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/todos/:todoId"
+        element={
+          <ProtectedRoute>
+            <TodoDetails />
+          </ProtectedRoute>
+        }
+      />
+    </Routes>
+  );
+}
